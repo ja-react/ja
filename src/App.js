aa7bf2cc3a3958f16936132b1df6c7e397e4864a
Szefncio <a@a.pl>
2020-03-14 02:34:27 +0100

    Test github2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Test</p>
+        <p>Test2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
